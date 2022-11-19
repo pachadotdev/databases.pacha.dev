9aaaf9cb6f9bcbdaff677c8b4589ffc153144159
Pacha <mavargas11@uc.cl>
2022-11-19 17:15:15 -0500

    add mastodon link, bye twitter

--- censo2017/04-copy-maps.R
+++ censo2017/04-copy-maps.R
@@ -1,4 +1,5 @@
 library(RPostgres)
+# library(RPostgreSQL) 
 library(dplyr)
 library(tidyr)
 library(sf)
@@ -9,9 +10,10 @@ tbl_names <- c("mapa_regiones", "mapa_provincias", "mapa_comunas",
 shp_names <- c("regiones_15r.shp", "provincias_15r.shp", "comunas_15r.shp", 
                "zonas_15r.shp", "calles_15r.shp")
 
-fun_con <- function(type) {
+fun_con <- function() {
   con <- dbConnect(
     Postgres(),
+    # PostgreSQL(),
     user = Sys.getenv("dbedu_usr"),
     password = Sys.getenv("dbedu_pwd"),
     dbname = "censo2017",
@@ -24,15 +26,17 @@ fun_con <- function(type) {
 for (j in seq_along(tbl_names)) {
   cat(paste0(tbl_names[j], "\n"))
   tictoc::tic()
-  d <- read_sf(paste0("~/github/censo2017-cartografias/", shp_names[j]))
-  con <- fun_con(select_engine)
-  dbWriteTable(
-    con, 
-    tbl_names[j],
-    d %>% 
-      ungroup(),
-    temporary = F, overwrite = F, append = T
-  )
+  d <- read_sf(paste0("~/github/censo2017-cartografias/", shp_names[j])) %>% 
+    ungroup()
+  con <- fun_con()
+  # dbWriteTable(
+  #   con, 
+  #   tbl_names[j],
+  #   d %>% 
+  #     ungroup(),
+  #   temporary = F, overwrite = F, append = T
+  # )
+  st_write(d, con, tbl_names[j])
   dbDisconnect(con)
   rm(d)
   tictoc::toc()
